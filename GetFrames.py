b7f931ed10a14a16c1dd4f4ab50b4da20e349c23
im-vijayts <vijayts2001@gmail.com>
2020-05-01 21:52:19 +0530

    Almost working need to fix os cmd

--- GetFrames.py
+++ GetFrames.py
@@ -1,13 +1,18 @@
 # To change the framerate of the video without changing the duration of the video
-# ffmpeg -i in.mp4 -r 10 -y in-15.mp4
+# ffmpeg -i in.mp4 -r 10 -y in-10.mp4
 
+import os
 import cv2 
+import p
+import ffmpeg
 
 '''
     Getting each frame of the input video and saving it to a folder called 'frames'
 '''
 
-input_file_name = 'in-10.mp4'
+os.popen('ffmpeg -i in.mp4 -r 10 -y in-10.mp4')
+
+input_file_name = os.getcwd() + '\\in-10.mp4'
 
 def FrameCapture(path): 
     vidObj = cv2.VideoCapture(path) 
@@ -15,7 +20,12 @@ def FrameCapture(path):
     success = 1
     while success: 
         success, image = vidObj.read() 
-        cv2.imwrite("./frames/frame%d.jpg" % count, image)
+        cv2.imwrite(os.getcwd() + '\\frames\\frame%d.jpg' % count, image)
         count += 1
-
-FrameCapture(input_file_name)
+try:
+    FrameCapture(input_file_name)
+except:
+    print(' oh no something went wrong ')
+finally:
+    p.imageposterizer()
+    p.createPosterizedVideo()
--- p.py
+++ p.py
@@ -3,35 +3,39 @@ from moviepy.editor import *
 import glob
 import os
 
-'''
-	Applying the posterizer effect
-	In the line,
+def imageposterizer():
+
+	'''
+		Applying the posterizer effect
+		In the line,
+			im2 = ImageOps.posterize(im1, 2)  
+		the 2 represents the intensity of posterization, highest being 1 and lowest being 8
+		
+		The output frames are saved to a folder called 'op'
+	'''
+
+	n = len(os.listdir('./frames'))
+
+	for i in range(n):
+		im1 = Image.open(f"./frames/frame{i}.jpg")  
+
 		im2 = ImageOps.posterize(im1, 2)  
-	the 2 represents the intensity of posterization, highest being 1 and lowest being 8
-	
-	The output frames are saved to a folder called 'op'
-'''
-
-n = len(os.listdir('./frames'))
-
-for i in range(n):
-	im1 = Image.open(f"./frames/frame{i}.jpg")  
-
-	im2 = ImageOps.posterize(im1, 2)  
-	
-	im2.save(f'./op/{i}.jpg')
+		
+		im2.save(f'./op/{i}.jpg')
 
 
-'''
-	Creating the final video File
-'''
+def createPosterizedVideo():
 
-n = len(os.listdir('./op'))
-img = [f'{i}.jpg' for i in range(n)]
-clips = [ImageClip(m).set_duration(0.1) for m in img]
+	'''
+		Creating the final video File
+	'''
 
-vidclip = VideoFileClip('in-10.mp4')
-audio = vidclip.audio
+	n = len(os.listdir('./op'))
+	img = [f'{i}.jpg' for i in range(n)]
+	clips = [ImageClip(m).set_duration(0.1) for m in img]
 
-concat_clip = concatenate_videoclips(clips, method="compose").set_audio(audio)
-concat_clip.write_videofile("test.mp4", fps=24)
+	vidclip = VideoFileClip('in-10.mp4')
+	audio = vidclip.audio
+
+	concat_clip = concatenate_videoclips(clips, method="compose").set_audio(audio)
+	concat_clip.write_videofile("test.mp4", fps=24)
